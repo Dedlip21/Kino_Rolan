1997cadf399d2c0a3fdc95b87ed3c8fde48bab4f
Opilane TTHK <opilane@tthk.ee>
2022-02-01 11:37:05 +0200

    Add project files.

---
+++ AdminMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kino_Rolan
+{
+    class AdminMenu
+    {
+        public FormStartPosition StartPosition { get; internal set; }
+        public Size ClientSize { get; private set; }
+
+        internal void Show()
+        {
+            this.ClientSize = new System.Drawing.Size(720, 500);
+
+        }
+    }
+}
